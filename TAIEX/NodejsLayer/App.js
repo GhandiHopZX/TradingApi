18ad833ebbc3fe1ce590cbaa1e2623eaac15d318
azuka <azukastyer@gmail.com>
2019-06-09 03:04:57 -0500

    ok

---
+++ TAIEX/NodejsLayer/App.js
@@ -0,0 +1,31 @@
+﻿import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Tapix</h1>
+                </header>
+                <p className="App-intro">
+                    Start Here, edit <code>src/App.js</code> and save to reload.
+                    </p>
+
+                <body>
+                    <header className="Marketo">
+                        <h6 datatype = {Math}>
+                            <h6 className="Ticker">
+                                <a href= "/*tradingViewReferece1*/" src = "" rel = ""> </a>
+                            </h6>
+                        </h6>
+                        </header>
+                    </body>
+            </div>
+        );
+    }
+}
+
+export default App;
