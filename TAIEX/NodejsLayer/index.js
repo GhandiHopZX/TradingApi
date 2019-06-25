2156a75ee8825f6d27f7cae3611cd95fabe0a4e0
azuka <azukastyer@gmail.com>
2019-06-25 17:01:33 -0500

    ok

--- TAIEX/NodejsLayer/index.js
+++ TAIEX/NodejsLayer/index.js
@@ -6,3 +6,15 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const express = require('express');
+
+const api = express();
+api.listen(3000, () => {
+    console.log('API active');
+});
+
+api.get('/', (req, res) => {
+    console.log(req);
+    res.send('hello world');
+});
